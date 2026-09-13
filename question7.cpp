b8c77ad15a98246bccc56327e120b16b4c6d8f2d
Rishab Kumar Jha <rishabkumarjha180@gmail.com>
2026-09-13 11:45:40 +0530

    Check Leap Year
    Write a program to determine if a given calendar year is a leap year. A year is a leap year if it is divisible by 4, except for end-of-century years (divisible by 100), which must also be divisible by 400.

---
+++ question7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int y;
+    cin >> y;
+    if ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0)) {
+        cout << y << " is a Leap Year" << endl;
+    } else {
+        cout << y << " is not a Leap Year" << endl;
+    }
+}
