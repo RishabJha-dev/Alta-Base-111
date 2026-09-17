6cf5002fc1730132b4428bcec0cc285ab65d186b
Rishab Kumar Jha <rishabkumarjha180@gmail.com>
2026-09-17 17:14:13 +0530

    Check Prime Number
    Write a program to determine whether a given integer N (N > 1) is a prime number or not. Optimize the loop to check divisibility only up to sqrt(N)

---
+++ Question15.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,c=1;
+    cout<<"enter a number :";
+    cin>>n;
+    for (int i=2;i<n;i++){
+        if (n%i==0){
+            cout<<"composite number";
+            c=0;
+            break;   
+        }
+
+    }
+    if (c==1) cout<<"prime number";
+    if (n==1) cout<<"neither a prime nor composite";
+}
