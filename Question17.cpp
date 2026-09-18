9ce6cacda27102ba21f32f11495cfca4c9435f20
Rishab Kumar Jha <rishabkumarjha180@gmail.com>
2026-09-18 09:12:59 +0530

    Fibonacci Series (Iterative)
    Write a program to generate and print the first N terms of the Fibonacci sequence iteratively. The series starts with 0 and 1, and each subsequent term is the sum of the previous two terms.

---
+++ Question17.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,n1,n2,n3;
+    cin>>n;
+    n1=0,n2=1;
+    for(int i=0 ; i<=n ; i++){
+        cout<<n1<<" ";
+        n3=n1+n2;
+        n1=n2;
+        n2=n3;
+    }
+}
